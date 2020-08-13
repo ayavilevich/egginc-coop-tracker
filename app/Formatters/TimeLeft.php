dbe1926e4b66a6820033488dd18631531681e376
Joshua Bixler <joshbixler@sbcglobal.net>
2020-08-13 16:46:27 -0500

    Fix Time left formatter issues

--- app/Formatters/TimeLeft.php
+++ app/Formatters/TimeLeft.php
@@ -9,29 +9,29 @@ class TimeLeft
 
         $seconds -= $days * 60 * 60 * 24;
 
-        $hours = floor($seconds / (60 * 60 * 24));
+        $hours = floor($seconds / (60 * 60));
         $seconds -= $hours * 60 * 60;
 
         $minutes = floor($seconds / 60);
         $seconds -= $minutes * 60;
 
-        $timeLeft = '';
+        $timeLeft = [];
         if ($days) {
-            $timeLeft .= $days . ' day' . ($days > 1 's' : '');
+            $timeLeft[] = $days . ' day' . ($days > 1 ? 's' : '');
         }
 
         if ($hours) {
-            $timeLeft .= $hours . ' hour' . ($hours > 1 's' : '');
+            $timeLeft[] = $hours . ' hour' . ($hours > 1 ? 's' : '');
         }
 
         if ($minutes) {
-            $timeLeft .= $minutes . ' minute' . ($minutes > 1 : 's' : '');
+            $timeLeft[] = $minutes . ' minute' . ($minutes > 1 ? 's' : '');
         }
 
         if ($seconds && $showSeconds) {
-            $timeLeft .= $seconds . ' second' . ($seconds > 1 : 's' : '');
+            $timeLeft[] = $seconds . ' second' . ($seconds > 1 ? 's' : '');
         }
 
-        return $timeLeft;
+        return implode(' ', $timeLeft);
     }
 }
