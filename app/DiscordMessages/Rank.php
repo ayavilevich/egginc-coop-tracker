ce6b8e601cd6d8df83aff8860b21084c8e279358
Joshua Bixler <joshbixler@sbcglobal.net>
2021-01-04 08:15:43 -0600

    Fix issue when user not found using rank command

--- app/DiscordMessages/Rank.php
+++ app/DiscordMessages/Rank.php
@@ -24,12 +24,12 @@ class Rank extends Base
         if (!$user->egg_inc_player_id) {
             return 'Egg Inc Player ID not set. Use `eb!set-player-id {id}` to set.';
         }
-        try {
-            $username = $user->getEggPlayerInfo()->userName;
-        } catch (UserNotFoundException $e) {
+        $playerInfo = $user->getEggPlayerInfo();
+        if (!$playerInfo) {
             return 'Invalid Egg Inc Player ID. Use `eb!set-player-id {id}` to set correct ID';
         }
 
+        $username = $user->getEggPlayerInfo()->userName;
         $soulEggs = $user->getSoulEggsFormattedAttribute();
         $goldenEggs = $user->getEggsOfProphecyAttribute();
         $farmerRole = $user->getPlayerEggRankAttribute();
